30234bccc29dca77b7b9ec996657827612d14adb
Krystian <krystianbezpalko@gmail.com>
2021-04-24 14:48:02 +0200

    list zakupów V.0

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -1,11 +1,1 @@
-lista={
-    "piekarnia":["chleb","bułki","pączki"],
-    "warzywniak":["marchew","seler","rukola"]
-}
-i=0
-for sklep in lista.keys():
-    for produkt in lista.get(sklep):
-        i=i+1
-        print(f"Idę do {sklep.upper()} i kupuję tam {produkt.upper()}")
-        
-print(f"W sumie kupuję {i} produktów")
+
