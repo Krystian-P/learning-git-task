668629a8b870a079f854d11d61285395b6d5f797
Krystian <krystianbezpalko@gmail.com>
2021-05-23 18:39:25 +0200

    Zadanie 4.4
    TODO: dla chętnych

--- Kalkulator.py
+++ Kalkulator.py
@@ -1,25 +1,27 @@
 import logging
-import sys
 
 def kalkulator(działanie, pierwsza_liczba, druga_liczba):
+
     if działanie==1:
         logging.info(f"Dodaję {pierwsza_liczba} oraz {druga_liczba}")
         wynik=pierwsza_liczba+druga_liczba
-        print(f"Twój wynik wynosi: {wynik}")
+
     elif działanie==2:
         logging.info(f"Odejmuję {pierwsza_liczba} oraz {druga_liczba}")
         wynik= pierwsza_liczba-druga_liczba
-        print(f"Twój wynik wynosi: {wynik}")
+
     elif działanie==3:
         logging.info(f"Mnożę {pierwsza_liczba} oraz {druga_liczba}")
         wynik= pierwsza_liczba*druga_liczba
-        print(f"Twój wynik wynosi: {wynik}")
+
     elif działanie==4:
         logging.info(f"Dzielę {pierwsza_liczba} oraz {druga_liczba}")
         wynik= pierwsza_liczba/druga_liczba
-        print(f"Twój wynik wynosi: {wynik}")
+
     else:
         logging.WARN("nie ma takiej opcji")
+    print(f"Twój wynik wynosi: {wynik}")
+
 if __name__=="__main__":
     logging.basicConfig(level=logging.INFO)
     calk=int(input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawania, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie:  "))
